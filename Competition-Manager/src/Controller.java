5ee6af6ba05fd0c35336de1dc08496284ba6a04d
mysteriousguenther <jw110@hdm-stuttgart.de>
2016-06-04 22:15:23 +0200

    Add files via upload

---
+++ Competition-Manager/src/Controller.java
@@ -0,0 +1,32 @@
+package fussball.view;
+
+import java.io.IOException;
+
+import fussball.Main;
+import javafx.fxml.FXML;
+
+public class Controller {
+	
+	private Main main = new Main();
+	
+	@FXML
+	public void setTeamManagementView() throws IOException {
+		main.setCenterContent("view/TeamManagement.fxml");
+	}
+	
+	@FXML
+	public void setHelpView() throws IOException {
+		main.setCenterContent("view/Help.fxml");
+	}
+	
+	@FXML
+	public void editTeam() throws IOException {
+		main.showNewScene("view/EditTeam.fxml", "Team bearbeiten");
+	}
+	
+	@FXML
+	public void setGame() throws IOException {
+		main.setCenterContent("view/Game.fxml");
+	}
+
+}
---
+++ Competition-Manager/src/Main.java
@@ -0,0 +1,54 @@
+package fussball;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private static BorderPane root;
+	private static Stage primaryStage;
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		
+		root = FXMLLoader.load(getClass().getResource("view/Menu.fxml"));
+		
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("Verwaltungsprogramm Schülerfußballturnier");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public void setCenterContent(String resource) throws IOException {
+		AnchorPane centerPane = FXMLLoader.load(getClass().getResource(resource));
+		root.setCenter(centerPane);
+	}
+	
+	public void showNewScene(String resource, String stage) throws IOException {
+		AnchorPane newPane = FXMLLoader.load(getClass().getResource(resource));
+		
+		Stage newStage = new Stage();
+		newStage.setTitle(stage);
+		newStage.initModality(Modality.APPLICATION_MODAL);
+		newStage.initOwner(primaryStage);
+		Scene newScene = new Scene(newPane);
+		newStage.setScene(newScene);
+		newStage.showAndWait();
+	}
+	
+	public void clearCenter() {
+		root.setCenter(null);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
