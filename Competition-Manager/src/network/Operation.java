cf79ad74cdba903c43956d1290393945c6824b6b
Tobias Wolf <tw067@hdm-stuttgart.de>
2016-06-05 11:27:41 +0200

    Described the Variables after the different operations

--- Competition-Manager/src/network/Operation.java
+++ Competition-Manager/src/network/Operation.java
@@ -1,10 +1,29 @@
 package network;
 
 public enum Operation {
-	GET_PLAYER, GET_TEAM, GET_GROUP, GET_MATCH, 
-	MATCHING_PLAYERS, MATCHING_TEAMS, MATCHING_GROUPS, MATCHING_MATCHES,
-	ADD_PLAYER, REMOVE_PLAYER,
-	UPDATE_PLAYER, UPDATE_TEAM, UPDATE_MATCH,
+	
+	/* PlayerManager */
+	GET_PLAYER,				//Integer id
+	MATCHING_PLAYERS,		//Player matchingPlayer; -1 / null as wildcard
+	ADD_PLAYER,				//Player newPlayer
+	REMOVE_PLAYER,			//Player oldPlayer
+	UPDATE_PLAYER,			//Player oldPlayer, Player changedPlayer
+	
+	/* TeamManager */
+	GET_TEAM,				//Integer id
+	MATCHING_TEAMS,			//Team matchingTeam; -1 / null as wildcard
+	UPDATE_TEAM,			//Team oldTeam, Team newTeam
+	
+	/* MatchManager*/
+	GET_MATCH,				//Integer id
+	MATCHING_MATCHES,		//Match matchingMatch; -1 / null as wildcard
+	UPDATE_MATCH,			//TODO
+	
+	/* GroupManager*/
+	GET_GROUP,  			//Integer id
+	MATCHING_GROUPS, 		//Group matchingGroup; -1 / null as wildcard
+	
+	/* other (TODO) */  
 	
 	CREATE_GROUPS
 }
