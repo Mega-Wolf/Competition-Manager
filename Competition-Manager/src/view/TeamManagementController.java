4c66d638fea7020a557c774093394972e39a0598
Janina <jw110@hdm-stuttgart.de>
2016-06-06 23:08:14 +0200

    Table Stuff etc.

--- Competition-Manager/src/view/TeamManagementController.java
+++ Competition-Manager/src/view/TeamManagementController.java
@@ -1,25 +1,25 @@
 package view;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import network.Operation;
 import other.Team;
 
 public class TeamManagementController {
-
-	
-
-	
-	
-	
-	
 	
 	Main main = new Main();
 	
-	
+	// Switching Scenes
 	@FXML
 	public void editTeam() throws IOException {
 		main.showNewScene("EditTeam.fxml", "Team bearbeiten");
@@ -30,14 +30,52 @@ public class TeamManagementController {
 		main.showNewScene("EditTeam.fxml", "Neues Team eintragen");
 	}
 	
+	public Team team;
+	int teamID;
+	
+	//Trying to read in team
+	public void loadingPlayer() throws UnknownHostException, IOException {
+		
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				int id = 0;
+				int PORT_NUMBER = 44532;
+				
+				Socket server;
+				try {
+					server = new Socket("127.0.0.1",PORT_NUMBER);
+					
+					try (ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream()); ObjectInputStream in = new ObjectInputStream(server.getInputStream());) {
+							out.writeObject(Operation.GET_TEAM);
+							out.writeObject(id);
+							
+							team = (Team) in.readObject();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private SimpleStringProperty teamSchoolProp;
+	private SimpleIntegerProperty teamIDProp;
+	
+		
+	//FXML Stuff
 	@FXML
 	private TableView<Team> teamTable;
 	
 	@FXML
-	private TableColumn<Team, Integer> showTeamID;
+	private TableColumn<TeamManagementController, Integer> showTeamID;
 	
 	@FXML
-	private TableColumn<Team, String> showTeamSchool;
+	private TableColumn<TeamManagementController, String> showTeamSchool;
 	
 	@FXML
 	private Label school;
@@ -51,9 +89,19 @@ public class TeamManagementController {
 	public TeamManagementController() {
 	}
 	
+	private SimpleStringProperty teamSchoolProp() {
+		teamSchoolProp.set(team.getSchool());
+		return teamSchoolProp;
+	}
+	
+	private SimpleIntegerProperty teamIDProp() {
+		teamIDProp.set(teamID);
+		return teamIDProp;
+	}
 	@FXML
 	private void setTableContent() {
 		
+		showTeamSchool.setCellValueFactory(cellData -> cellData.getValue().teamSchoolProp());
 	}
 	
 	
