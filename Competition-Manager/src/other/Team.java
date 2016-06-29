dd06c5bd6a2401257ce32c0b58a4848e51f79f79
Tobias Wolf <tw067@hdm-stuttgart.de>
2016-06-29 20:50:40 +0200

    Added abbreviation

--- Competition-Manager/src/other/Team.java
+++ Competition-Manager/src/other/Team.java
@@ -1,72 +1,36 @@
 package other;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Team{
-		
+	
 	/* Consts */
 	public static final int TEAM_SIZE_MAX = 23;
 	public static final int TEAM_SIZE_MIN = 11;
 	
 	/* Variables */
-	private final int id;
-	private String school;
+	private final String school;
+	private final String abbreviation;
 	
 	/* Constructor */
-	public Team(int id, String school) {
-		this.id = id;
+	public Team(String school, String abbreviation) {
 		this.school = school;
+		this.abbreviation = abbreviation;
 	}
-	
-	/* Methods */
-	/*
-	public void addPlayer(Player player) {
-		if (playerList.size() < TEAM_SIZE_MAX) {
-			
-			boolean numberFree = true;
-			
-			for (Player p : playerList) {
-				if (p.getNumber() == player.getNumber()) {
-					numberFree = false;
-					break;
-				}
-			}
-			
-			if (numberFree) {
-				playerList.add(player);
-				//TODO; Logging
-			}
-			else {
-				//TODO; Exception, bzw Fehler
-			}
-			
-			
-
-		} else {
-			//TODO; Exception
-		}
-	}
-	
-	public void deletePlayer(Player player) {
-		
-		if (playerList.contains(player)) {
-			playerList.remove(player);
-			//TODO: logging
-		}
-		else {
-			//TODO: Exception
-		}
-	}*/
-	
-	/* Setter */
 	
 	/* Getter */
 	public String getSchool() {
 		return school;
 	}
-	/*
-	public int getNumberOfPlayers() {
-		return playerList.size();
+	
+	public String getAbbreviation() {
+		return abbreviation;
 	}
-	*/
+	
+	/* Overrides */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Team) {
+			return ((Team) obj).school.equals(school);
+		}
+		return false;
+	}
 }
