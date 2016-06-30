fa17f8add814bceae6429d5d400e8b1dba685156
Tobias Wolf <tw067@hdm-stuttgart.de>
2016-06-30 15:51:00 +0200

    Still a lot todo, but commit anyway

--- Competition-Manager/src/match/GroupMatch.java
+++ Competition-Manager/src/match/GroupMatch.java
@@ -6,25 +6,72 @@ import other.Team;
 import other.Group;
 
 public class GroupMatch extends Match {
-
+	
+	/* Consts */
+	public static final int POINTS_WINNER = 3;
+	public static final int POINTS_LOSER = 0;
+	public static final int POINTS_DRAW = 1;
+	
 	/* Variables */
-	private Group group;
+	private final int groupID;
 	
 	/* Constructors */
-	public GroupMatch(Team team0, Team team1, Date kickoff, Group group) {
-		super(team0, team1, kickoff);
-		this.group = group;
-	}
-	
-	public GroupMatch(Team team0, Team team1, Date kickoff, Group group, int goals0, int goals1) {
-		this(team0, team1, kickoff, group);
-		goals[0] = goals0;
-		goals[1] = goals1;
+	public GroupMatch(int[] teamIDs, Date kickoff, int groupID) {
+		super(teamIDs, kickoff);
+		this.groupID = groupID;
 	}
 	
 	/* Getter */
-	public Group getGroup() {
-		return group;
+	public int getGroupID() {
+		return groupID;
+	}
+	
+	public int[] getPoints() {
+		//TODO: TEST
+		
+		int[] points = new int[2];
+		
+		if (goalsRegular[0] == goalsRegular[1]) {
+			points[0] = POINTS_DRAW;
+			points[1] = POINTS_DRAW;
+		} else {
+			if (goalsRegular[0] > goalsRegular[1]) {
+				points[0] = POINTS_WINNER;
+				points[1] = POINTS_LOSER;
+			} else {
+				points[0] = POINTS_LOSER;
+				points[1] = POINTS_WINNER;
+			}
+		}
+		
+		return points;
+	}
+	
+	/*
+	 * int[] ids = {1,2};
+	 * int groupId = 1;
+	 * int[] goals = {2,1};
+	 * GroupMatch g = new GroupMatch(ids, new Date(), groupId, goals);
+	 * 
+	 * int[] expectedResult = {GroupMatch.POINTS_WINNER, GroupMatch.POINTS_LOSER}
+	 * 
+	 * Assert.assertEquals( g.getPoints() ,  expectedResult );
+	 */
+	
+	public int getPoints(int teamID) {
+		//TODO: TEST
+		if (goalsRegular[0] == goalsRegular[1]) {
+			if (teamIDs[0] == teamID || teamIDs[1] == teamID) {
+				return POINTS_DRAW;
+			}
+		} else {
+			if ((teamIDs[0] == teamID && goalsRegular[0] > goalsRegular[1]) || (teamIDs[1] == teamID && goalsRegular[1] > goalsRegular[0])) {
+				return POINTS_WINNER;
+			} else {
+				return POINTS_LOSER;
+			}
+		}
+		return 0; //Should not be reached
 	}
 
 }
--- Competition-Manager/src/match/Match.java
+++ Competition-Manager/src/match/Match.java
@@ -7,37 +7,46 @@ import other.Team;
 public abstract class Match {
 	
 	/* Variables */
-	protected Team teams[] = new Team[2];
-	protected int goals[] = new int[2];
+	protected int teamIDs[] = new int[2];
+	protected int goalsRegular[] = new int[2];
 	protected Date kickoff;
 	
 	/* Constructors */
-	public Match(Team team0, Team team1, Date kickoff) {
-		teams[0] = team0;
-		teams[1] = team1;
+	public Match(int teamIDs[], Date kickoff) {
+		this.teamIDs = teamIDs;
+		//TODO; chekArrays?
 		this.kickoff = kickoff;
 	}
 	
-	public Match(Team team0, Team team1, Date kickoff, int goals0, int goals1) {
-		this(team0, team1, kickoff);
-		goals[0] = goals0;
-		goals[1] = goals1;
-	}
-	
 	/* Setter */
-	public void setGoals(int goals0, int goals1) {
-		goals[0] = goals0;
-		goals[1] = goals1;
+	public void setGoalsRegular(int goalsRegular0, int goalsRegular1) {
+		//passed as ints instead of array; therefore no check is needed
+		//does not check for negative goals, so other games could be supported as well
+		//TODO; should thnk about that again
+		this.goalsRegular[0] = goalsRegular0;
+		this.goalsRegular[1] = goalsRegular1;
 	}
 	
 	/* Getter */
-	public int[] getGoals() {
-		return Arrays.copyOf(goals, 2);
+	public int[] getGoalsRegular() {
+		return Arrays.copyOf(goalsRegular, 2);			// TODO; only used by server; should be no problem to pass the reference
+	}
+	
+	public int getGoalsRegular(int teamID) {
+		if (teamIDs[0] == teamID) {
+			return goalsRegular[0];
+		}
+		if (teamIDs[1] == teamID) {
+			return goalsRegular[1];
+		}
+		
+		return 0;	//team was not part of the match
 	}
 
-	public Team[] getTeams() {
-		return Arrays.copyOf(teams, 2);
+	public int[] getTeamIDs() {
+		return Arrays.copyOf(teamIDs, 2);				// TODO; only used by server; should be no problem to pass the reference
 	}
+
 	
 	public Date getKickoff() {
 		return kickoff;
