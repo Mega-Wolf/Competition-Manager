d659705be8878f2228898cd74ad3916417b289e6
Tobias Wolf <tw067@hdm-stuttgart.de>
2016-07-03 09:38:18 +0200

    forgot to commit that

---
+++ Competition-Manager/src/test/MatchRound.java
@@ -0,0 +1,23 @@
+package test;
+
+public class MatchRound extends MatchExtension{
+
+	private int[] goalsOvertime;
+	private int[] goalsPenalty;
+	
+	/* Constructor */
+	public MatchRound(int[] goalsRegular) {
+		super(goalsRegular);
+		//TODO; Exception, if g0 == g1; if ![2]
+	}
+	
+	public MatchRound(int[] goalsRegular, int[] goalsOvertime) {
+		this(goalsRegular);
+		//TODO; Eception, if g0 != g1; if ![2]; go0+gO0 == g1+gO1
+	}
+	public MatchRound(int[] goalsRegular, int[] goalsOvertime, int[] goalsPenalty) {
+		this(goalsRegular, goalsOvertime);
+		//TODO; Eception, if g0 != g1; if ![2]; go0+gO0 != g1+gO1 
+	}
+	
+}
