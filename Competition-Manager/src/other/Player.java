8836645f272329dc5dc7ae70fb7f0802a4ec2483
Tobias Wolf <tw067@hdm-stuttgart.de>
2016-07-03 17:30:51 +0200

    Addec Serializable and Javadocs

--- Competition-Manager/src/other/Player.java
+++ Competition-Manager/src/other/Player.java
@@ -1,49 +1,114 @@
 package other;
-public class Player implements EqualWildCard{
+
+import java.io.Serializable;
+
+public class Player implements EqualWildCard, Serializable {
+	
+	/* Consts */
+	
+	/**
+	 * needed for serialization
+	 */
+	private static final long serialVersionUID = 4487365076046935560L;
 	
 	/* Variables */
-	private int number;
-	private int team;
-	private String forename;
-	private String surname;
 	
+	/**
+	 * trikot-number of player; 1 - 99
+	 */
+	private int number;
+
+	/**
+	 * id of the player's team
+	 */
+	private int team;
+
+	/**
+	 * forename of the player; not checked, whether sensible or not
+	 */
+	private String forename;
+
+	/**
+	 * surname of the player; not checked, whether sensible or not
+	 */
+	private String surname;
+
 	/* Constructor */
+
+	/**
+	 * Create a player
+	 * 
+	 * @param number
+	 *            trikot-number between 1 and 99
+	 * @param team
+	 *            team ID; 0 or above; references {@link Team}
+	 * @param forename
+	 *            neither null nor empty
+	 * @param surname
+	 *            neither null nor empty
+	 */
 	public Player(int number, int team, String forename, String surname) {
-		
-		//if ((number > 0 && number < 100) && forename.) {
-			this.number = number;
-			this.team = team;
-			this.forename = forename;
-			this.surname = surname;
-		//}
-		
-		
-		
+		this.number = number;
+		this.team = team;
+		this.forename = forename;
+		this.surname = surname;
+
+		if (number <= 0 || number >= 100) {
+			throw new IllegalArgumentException("Trikot-number must be between 1 and 99");
+		}
+		if (team < 0) {
+			throw new IllegalArgumentException("Team ID must be 0 or above");
+		}
+		if (forename == null || forename.length() == 0) {
+			throw new IllegalArgumentException("Empty forename is not allowed");
+		}
+		if (surname == null || surname.length() == 0) {
+			throw new IllegalArgumentException("Empty surname is not allowed");
+		}
+
 	}
-		
+
 	/* Getter */
+
+	/**
+	 * 
+	 * @return trikot-number; between 1 and 99
+	 */
 	public int getNumber() {
 		return number;
 	}
-	
+
+	/**
+	 * 
+	 * @return teamID; 0 or above
+	 */
 	public int getTeam() {
 		return team;
 	}
-	
+
+	/**
+	 * 
+	 * @return forename; neither null nor empty
+	 */
 	public String getForename() {
 		return forename;
 	}
-	
+
+	/**
+	 * 
+	 * @return surname; neither null nor empty
+	 */
 	public String getSurname() {
 		return surname;
 	}
-	
+
 	/* Overrides */
 	@Override
-	public boolean equals(Object obj) {		
+	public boolean equals(Object obj) {
 		if (obj instanceof Player) {
 			Player test = (Player) obj;
-			return (number == test.number) && (team == test.team) && (surname.equals(test.surname)) && (forename.equals(test.forename));
+			return (number == test.number) && (team == test.team) && (surname.equals(test.surname))
+					&& (forename.equals(test.forename));
 		}
 		return false;
 	}
@@ -52,7 +117,9 @@ public class Player implements EqualWildCard{
 	public boolean equalsWC(Object obj) {
 		if (obj instanceof Player) {
 			Player test = (Player) obj;
-			return (test.number == -1 || number == test.number) && (test.team == -1 || team == test.team) && (test.surname == null || surname.equals(test.surname)) && (test.forename == null || forename.equals(test.forename));
+			return (test.number == -1 || number == test.number) && (test.team == -1 || team == test.team)
+					&& (test.surname == null || surname.equals(test.surname))
+					&& (test.forename == null || forename.equals(test.forename));
 		}
 		return false;
 	}
--- Competition-Manager/src/other/Team.java
+++ Competition-Manager/src/other/Team.java
@@ -1,26 +1,70 @@
 package other;
 
-public class Team implements EqualWildCard {
+import java.io.Serializable;
 
+public class Team implements EqualWildCard, Serializable {
+	
 	/* Consts */
+	
+	/**
+	 * needed for serialization
+	 */
+	private static final long serialVersionUID = 2189452111107525742L;
+	
+	/**
+	 * maximum number of players in a team
+	 */
 	public static final int TEAM_SIZE_MAX = 23;
+	
+	/**
+	 * minimum number of players in a team
+	 */
 	public static final int TEAM_SIZE_MIN = 11;
 
 	/* Variables */
+	
+	/**
+	 * the full name of the school
+	 */
 	private final String school;
+	
+	/**
+	 * an abbreviation of the school
+	 */
 	private final String abbreviation;
 
 	/* Constructor */
+	/**
+	 * Creates a Team object
+	 * @param school the full name of the school; neither null nor empty
+	 * @param abbreviation abbreviation of the school's name; neither null nor empty; should be 2-5 characters
+	 */
 	public Team(String school, String abbreviation) {
 		this.school = school;
 		this.abbreviation = abbreviation;
+		
+		if (school == null || school.length() == 0) {
+			throw new IllegalArgumentException("Empty schoolname is not allowed");
+		}
+		if (abbreviation == null || abbreviation.length() == 0) {
+			throw new IllegalArgumentException("Empty abbreviation is not allowed");
+		}
 	}
 
 	/* Getter */
+	
+	/**
+	 * 
+	 * @return schoolname; neither null nor empty
+	 */
 	public String getSchool() {
 		return school;
 	}
-
+	
+	/**
+	 * 
+	 * @return abbreviation; neither null nor empty
+	 */
 	public String getAbbreviation() {
 		return abbreviation;
 	}
