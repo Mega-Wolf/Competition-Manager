3809e53fbb9f55cf61ac3b7048b00dbc5334669b
VenomousVali <ryukohatsumei@hotmail.de>
2016-07-04 15:22:39 +0200

    Manager tests, map ... getMatching

---
+++ Competition-Manager/Test/other/ManagerTest.java
@@ -0,0 +1,32 @@
+package other;
+
+import static org.junit.Assert.*;
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class ManagerTest {
+
+	@Test
+	public void Test1() {
+		Manager<Player> playerManager = new Manager<Player>();
+		playerManager.add(new Player(3,0,"Herbert","Grönemeier"));
+		playerManager.add(new Player(4,0,"Günther","Jauch"));
+		Map<Integer,Player> = playerManager.getMatching(new Player(-1,1,null,null));
+	}
+	
+	@Test
+	public void Test2() {
+		Manager<Player> playerManager = new Manager<Player>();
+		playerManager.add(new Player(1,1,"Pablo","Picasso"));
+		playerManager.add(new Player(2,1,"Salvador","Dali"));
+		Map<Integer,Player> = playerManager.getMatching(new Player(-1,1,null,null));
+	}
+/*
+		playerManager.add(new Player(-2,3,"Anabel","Lee"));
+		playerManager.add(new Player(2,-1,"Walls","of Jericho"));
+		playerManager.add(new Player(0,1,"Bastian","Schweini"));
+*/
+
+}
