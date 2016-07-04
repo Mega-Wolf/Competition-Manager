c89546980c3e69c6752f8df3fc20a851584e41b0
VenomousVali <ryukohatsumei@hotmail.de>
2016-07-04 15:41:17 +0200

    further fixes

--- Competition-Manager/Test/other/ManagerTest.java
+++ Competition-Manager/Test/other/ManagerTest.java
@@ -3,6 +3,7 @@ package other;
 import static org.junit.Assert.*;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.junit.Test;
 
@@ -14,6 +15,9 @@ public class ManagerTest {
 		playerManager.add(new Player(3,0,"Herbert","Grönemeier"));
 		playerManager.add(new Player(4,0,"Günther","Jauch"));
 		Map<Integer,Player> meineDummeBehinderteListe = playerManager.getMatching(new Player(-1,1,null,null));
+		assertTrue(meineDummeBehinderteListe.size() == 2
+				&& meineDummeBehinderteListe.entrySet().contains("Herbert Grönemeier")
+				&& meineDummeBehinderteListe.entrySet().contains("Günther Jauch"));
 	}
 	
 	@Test
@@ -22,11 +26,17 @@ public class ManagerTest {
 		playerManager.add(new Player(1,1,"Pablo","Picasso"));
 		playerManager.add(new Player(2,1,"Salvador","Dali"));
 		Map<Integer,Player> meineAndereDummeBehinderteListe = playerManager.getMatching(new Player(-1,1,null,null));
+		assertTrue(meineAndereDummeBehinderteListe.size() == 2
+				&& meineAndereDummeBehinderteListe.entrySet().contains("Pablo Picasso")
+				&& meineAndereDummeBehinderteListe.entrySet().contains("Salvador Dali"));
 	}
 /*
+ 	@Test
+ 	public void Test3() {
 		playerManager.add(new Player(-2,3,"Anabel","Lee"));
 		playerManager.add(new Player(2,-1,"Walls","of Jericho"));
 		playerManager.add(new Player(0,1,"Bastian","Schweini"));
-*/
+ 	}
+ */
 
 }
