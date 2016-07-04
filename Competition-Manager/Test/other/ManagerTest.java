d5af1384b22b1c1fda53b3ee6e6e3655599fab10
VenomousVali <ryukohatsumei@hotmail.de>
2016-07-04 16:03:08 +0200

    sooo pretty but fail

--- Competition-Manager/Test/other/ManagerTest.java
+++ Competition-Manager/Test/other/ManagerTest.java
@@ -12,23 +12,27 @@ public class ManagerTest {
 	@Test
 	public void Test1() {
 		Manager<Player> playerManager = new Manager<Player>();
-		playerManager.add(new Player(3,0,"Herbert","Grönemeier"));
-		playerManager.add(new Player(4,0,"Günther","Jauch"));
-		Map<Integer,Player> List1 = playerManager.getMatching(new Player(3,0,null,null));
+		Player p1 = new Player(3,0,"Herbert","Grönemeier");
+		Player p2 = new Player(4,0,"Günther","Jauch");
+		playerManager.add(p1);
+		playerManager.add(p2);
+		Map<Integer,Player> List1 = playerManager.getMatching(new Player(-1,1,null,null));
 		assertTrue(List1.size() == 2
-				&& List1.entrySet().contains("Herbert Grönemeier")
-				&& List1.entrySet().contains("Günther Jauch"));
+				&& List1.entrySet().contains(p1)
+				&& List1.entrySet().contains(p2));
 	}
 	
 	@Test
 	public void Test2() {
 		Manager<Player> playerManager = new Manager<Player>();
-		playerManager.add(new Player(1,1,"Pablo","Picasso"));
-		playerManager.add(new Player(2,1,"Salvador","Dali"));
-		Map<Integer,Player> List2 = playerManager.getMatching(new Player(1,1,null,null));
+		Player p3 = new Player(1,1,"Pablo","Picasso");
+		Player p4 = new Player(2,1,"Salvador","Dali");
+		playerManager.add(p3);
+		playerManager.add(p4);
+		Map<Integer,Player> List2 = playerManager.getMatching(new Player(-1,1,null,null));
 		assertTrue(List2.size() == 2
-				&& List2.entrySet().contains("Pablo Picasso")
-				&& List2.entrySet().contains("Salvador Dali"));
+				&& List2.entrySet().contains(p3)
+				&& List2.entrySet().contains(p4));
 	}
 /*
  	@Test
