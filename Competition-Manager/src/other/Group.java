e7ae0d3bca7ca410e734628814c5e4a974d85fc0
Tobias Wolf <tw067@hdm-stuttgart.de>
2016-07-05 07:49:42 +0200

    Few minor changes

--- Competition-Manager/src/other/Group.java
+++ Competition-Manager/src/other/Group.java
@@ -1,9 +1,12 @@
 package other;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import match.GroupMatch;
+import match.MatchGroup;
 
-public class Group implements Serializable {
+public class Group implements EqualWildCard, Serializable {
 	
 	/* Consts */
 	
@@ -28,26 +31,68 @@ public class Group implements Serializable {
 	 */
 	public Group(int teamIDs[]) {
 		this.teamIDs = teamIDs;
-		//checkIDs();
 	}
 	
-	/* Methods */
-	/*private void checkIDs() {
-		if (teamIDs.length == 4) {
-			if (teamIDs[0] == teamIDs[1] || teamIDs[0] == teamIDs[2] || teamIDs[0] == teamIDs[3] || teamIDs[1] == teamIDs[2] || teamIDs[1] == teamIDs[3] || teamIDs[2] == teamIDs[3]) {
-				//TODO: Exception
-			}
-		}
-	}*/
+	// TODO
+	public void updateTable(List<MatchGroup> groupMatch) {
+		int ids[] = groupMatch.getTeamIDs();
+	}
 	
-	public void updateTable(GroupMatch match) {
-		int ids[] = match.getTeamIDs();
+	/* Getter */
+	public int getTeamID(int position) {
+		if (position < 0 || position >= 4) {
+			throw new IllegalArgumentException("Only indices 0 to 3 are allowed");
+		}
+		return teamIDs[position];
 	}
 	
 	
 	//TODO: DONO
 	private class GroupStat {
 		
+	}
+
+	/* Overrides */
+	
+	@Override
+	public boolean equalsWC(Object obj) {
+		if (obj instanceof Group) {
+			Group test = (Group) obj;
+			if (test.teamIDs == null) {
+				return true;
+			}
+			for (Integer i: test.teamIDs) {
+				boolean in = false;
+				for (Integer j: this.teamIDs) {
+					if (i.equals(j)) {
+						in = true;
+						break;
+					}
+				}
+				if (!in) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isValid() {
+		if (teamIDs.length != 4) {
+			return false;
+		}
+		Set<Integer> set = new HashSet<Integer>();
+		for (Integer i : teamIDs) {
+			set.add(i);
+		}
+		
+		if (set.size() != 4) {
+			return false;
+		}
+		
+		return true;
 	}
 	
 }
